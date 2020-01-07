5f8db1f7ade577f50f940687f10f529786e7c9f8
Vincent Nguyen <vpvnguyen@gmail.com>
2020-01-07 09:20:31 -0800

    turn new array back to string

--- exercises/capitalize/notes.js
+++ exercises/capitalize/notes.js
@@ -6,16 +6,23 @@
 // - join first letter with rest of the string
 // - push result into 'words' array
 // return joined 'words' into a string
+
+// Example
 const string = 'Hi there it is me';
 
 // make an empty array 'words'
 const words = [];
+
+// split the input string by spaces to get an array; pass in a space to split by space
+const splitString = string.split(' ');
+console.log(`string to array: ${splitString}`);
 
 // for each word in the array
 splitString.forEach(item => {
 
     // split item (string) to array
     const array = item.split('');
+
     // split string into an array; shift (first item) to isolate and make uppercase
     const firstCharacerUpperCase = item.split('').shift().toUpperCase();
     console.log(`first character uppercase: ${firstCharacerUpperCase}`);
@@ -23,16 +30,16 @@ splitString.forEach(item => {
     // my_array.splice(start_index, number_of_elements_to_remove, NUM TO INSERT);
     // put firstCharacterUpperCase back into array index [0], 1 value to remove, with the firstCharacterUppercase
     array.splice(0, 1, firstCharacerUpperCase);
-
     console.log(`new array: ${array}`); // [H,i] [T,h,e,r,e]
 
     // join new item array back into string
     const arrayToString = array.join('');
-
-    console.log(`joined array: ${arrayToString}`)
+    console.log(`joined array: ${arrayToString}`);
 
     // push all new group of words into words array
     words.push(arrayToString);
 });
 
-console.log(`${words}`); // [Hi, There, It, Is, Me]
+const wordsToString = words.join(' ');
+console.log(`Answer: ${wordsToString}`);
+console.log(typeof wordsToString);
