5590c28d2097b205d7e857eb0bb60435a0b6d02a
Vincent Nguyen <vpvnguyen@gmail.com>
2020-01-07 09:29:18 -0800

    solved - find first character of string, iterate and find the ' ' spaces, uppercase the character next to the space, return result

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,28 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+
+    // set result as the capitalized first character of string
+    // you can read index of a string using [i]
+    let result = str[0].toUpperCase();
+
+    // iterate from 1, to the length of the string
+    for (let i = 1; i < str.length; i++) {
+
+        // look to left of current character
+        // if it is a space, then take current character we are looking at, uppercase it, and add it on to the result string 
+        if (str[i - 1] === ' ') {
+            // add uppercase to result
+            result += str[i].toUpperCase();
+        } else {
+            // add unmodified character back to result
+            result += str[i];
+        }
+    }
+
+    console.log(`returned result: ${result}`);
+    return result;
+}
 
 module.exports = capitalize;
