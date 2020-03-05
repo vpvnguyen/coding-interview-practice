2833790d48b6fa7635e8cdde2703b8e866eee267
Vincent Nguyen <vpvnguyen@gmail.com>
2020-03-04 20:30:14 -0800

    attempt to solve with O(n)

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -3,17 +3,51 @@
 // Events class.  The Events class should
 // have methods 'on', 'trigger', and 'off'.
 
+// class Events {
+//   // Register an event handler
+//   on(eventName, callback) {}
+
+//   // Trigger all callbacks associated
+//   // with a given eventName
+//   trigger(eventName) {}
+
+//   // Remove all event handlers associated
+//   // with the given eventName
+//   off(eventName) {}
+// }
+
+// module.exports = Events;
+
+
 class Events {
+  constructor() {
+    this.events = {};
+  }
+  
   // Register an event handler
-  on(eventName, callback) {}
+  on(eventName, callback) {
+    if (this.events[eventName]) {
+      this.events[eventName].push(callback);
+    } else {
+      this.events[eventName] = [callback];
+    }
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    if (this.events[eventName]) {
+      for (let cb of this.events[eventName]) {
+        cb();
+      }
+    }
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    delete this.events[eventName];
+  }
 }
 
 module.exports = Events;
