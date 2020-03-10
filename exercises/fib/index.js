e56914772c8da4f327bb773ddd7c89f3c9e03344
Vincent Nguyen <vpvnguyen@gmail.com>
2020-03-09 22:48:31 -0700

    solved using while loop

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,22 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+  let a = 1,
+    b = 0,
+    temp;
+
+  while (n >= 0) {
+    temp = a;
+
+    a = a + b;
+    b - temp;
+    n--;
+  }
+
+  return b;
+}
+
+console.log(fib(4));
 
 module.exports = fib;
