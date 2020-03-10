41aea15c5695c1395a0033c47d933cac9449e6d0
Vincent Nguyen <vpvnguyen@gmail.com>
2020-03-09 23:16:49 -0700

    o(n) using recursive method

--- exercises/fib/notes.js
+++ exercises/fib/notes.js
@@ -5,13 +5,22 @@
 // - no - add up to n sequences starting from [0, 1]
 
 const fib = n => {
-  if (n == 1) {
-    return [0, 1];
-  } else {
-    const s = fib(n - 1);
-    s.push(s[s.length - 1] + s[s.length - 2]);
-    return s;
-  }
+  if (n == 1) return [0, 1];
+  const s = fib(n - 1);
+  s.push(s[s.length - 1] + s[s.length - 2]);
+  return s;
 };
 
 console.log(fib(8));
+
+// another way using recursion
+function fib2(n) {
+  if (n == 1) return [0, 1];
+
+  var arr = fib2(n - 1);
+  arr.push(arr[n - 1] + arr[n - 2]);
+
+  return arr;
+}
+
+console.log(fib2(8));
