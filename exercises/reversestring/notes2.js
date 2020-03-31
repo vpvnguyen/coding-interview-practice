3a2a27a4b032657d365b60f4e90daa2af672750f
Vincent Nguyen <vpvnguyen@gmail.com>
2020-03-30 17:16:57 -0700

    solved O(n) - es6 formatted, split, reverse, join

---
+++ exercises/reversestring/notes2.js
@@ -0,0 +1,17 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+const reverse = str =>
+  str
+    .split("")
+    .reverse()
+    .join("");
+
+console.log(reverse("apple"));
+console.log(reverse("hello"));
+console.log(reverse("Greetings!"));
